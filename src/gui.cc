f07d2bab87bda1ec4e2664a03e1d8c3215dfa6e0
Anton Novoselov <ans.novoselov@gmail.com>
2017-06-21 13:12:30 +0300

    Skeleton of gui

---
+++ src/gui.cc
@@ -0,0 +1,17 @@
+// Package: wumpus_game (v0.9)
+// Description: https://github.com/ans-hub/wumpus_game
+// Author: Anton Novoselov, 2017
+// File: gui implementation of wumpus_game
+
+#include "gui.h"
+
+namespace anshub {
+
+}  // namespace anshub
+
+int main()
+{
+  Fl_Window window(200, 200, "Window title");
+  window.show();
+  return Fl::run();
+}
---
+++ src/gui.h
@@ -0,0 +1,17 @@
+// Package: wumpus_game (v0.9)
+// Description: https://github.com/ans-hub/wumpus_game
+// Author: Anton Novoselov, 2017
+// File: interface to gui of wumpus_game
+
+#ifndef GUI_H
+#define GUI_H
+
+#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Window.H>
+
+namespace anshub {
+
+}  // namespace anshub
+
+#endif  // GUI_H
